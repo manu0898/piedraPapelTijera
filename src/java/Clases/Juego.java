23c4f288384a4b3578de02e26244d7a658920029
Manu <manuelruizgonzalez.1998@gmail.com>
2019-11-11 09:41:54 +0100

    Añadidos comentarios a la clase Juego

--- src/java/Clases/Juego.java
+++ src/java/Clases/Juego.java
@@ -6,13 +6,20 @@ package Clases;
  */
 public class Juego {
 
+    /**
+     * Constructor de la clase
+     */
     public Juego() {
     }
 
+    /**
+     * Método para elegir aleatoriamente la apuesta que realiza la máquina
+     * @return apuesta de la máquina
+     */
     public String apuesta() {
         String maquina = "";
         int aux = (int) (Math.random() * 5 + 1);
-        //apuesta de la maquina
+        
         switch (aux) {
             case 1:
                 maquina = "piedra";
@@ -33,9 +40,17 @@ public class Juego {
         return maquina;
     }
 
+    /**
+     * Método para determinar el ganador en función de la elección del jugador
+     * y la de la máquina.
+     * @param jugador elección del jugador
+     * @param maquina elección de la máquina
+     * @return resultado del enfrentamiento entre jugador y máquina
+     */
     public String jugada(String jugador, String maquina) {
         String resultado = "";
-        //jugador elige tijera
+        
+        //jugador elige tijera: Si la máquina elige lagarto o papel, gana; si elige spock o piedra, pierde; si elige tijera, empatan
         if (jugador.equals("tijera") && maquina.equals("lagarto") || jugador.equals("tijera") && maquina.equals("papel")) {
             resultado = "Has ganado!";
         } else {
@@ -48,7 +63,7 @@ public class Juego {
             }
         }
 
-        //jugador elige papel
+        //jugador elige papel: Si la máquina elige piedra o spock, gana; si elige lagarto o tijera, pierde; si elige papel, empatan
         if (jugador.equals("papel") && maquina.equals("piedra") || jugador.equals("papel") && maquina.equals("spock")) {
             resultado = "Has ganado!";
         } else {
@@ -61,7 +76,7 @@ public class Juego {
             }
         }
 
-        //jugador elige piedra
+        //jugador elige piedra: Si la máquina elige lagarto o tijera, gana; si elige spock o papel, pierde; si elige piedra, empatan
         if (jugador.equals("piedra") && maquina.equals("lagarto") || jugador.equals("piedra") && maquina.equals("tijera")) {
             resultado = "Has ganado!";
         } else {
@@ -74,7 +89,7 @@ public class Juego {
             }
         }
 
-        //jugador elige lagarto
+        //jugador elige lagarto: Si la máquina elige spock o papel, gana; si elige tijera o piedra, pierde; si elige lagarto, empatan
         if (jugador.equals("lagarto") && maquina.equals("papel") || jugador.equals("lagarto") && maquina.equals("spock")) {
             resultado = "Has ganado!";
         } else {
@@ -87,7 +102,7 @@ public class Juego {
             }
         }
 
-        //jugador elige spock
+        //jugador elige spock: Si la máquina elige tijera o piedra, gana; si elige papel o lagarto, pierde; si elige spock, empatan
         if (jugador.equals("spock") && maquina.equals("tijera") || jugador.equals("spock") && maquina.equals("piedra")) {
             resultado = "Has ganado!";
         } else {
